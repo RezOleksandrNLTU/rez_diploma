28b32fe41fb96999d1ce00f736368f0d5a8d3f34
RezOleksandr <rezoleksandr958@gmail.com>
2023-06-12 03:04:40 +0300

    Remove old views

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,8 +1,6 @@
-from django.urls import path, re_path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from rest_framework import routers
 
-from . import views
 from . import api
 
 
@@ -15,14 +13,4 @@ router.register(r'users', api.UserViewSet, basename='user')
 urlpatterns = [
     path('accounts/google/login/callback/', api.google_login_callback, name='google_login_callback'),
     path('api/', include(router.urls)),
-
-    path(r'', views.home, name='home'),
-    path(r'login', auth_views.LoginView.as_view(template_name='messenger/login.html', next_page='home'), name='login'),
-    path(r'logout', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path(r'signup', views.signup, name='signup'),
-    path(r'account_activation_sent', views.account_activation_sent, name='account_activation_sent'),
-    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})$', views.activate,
-            name='activate'),
-    path('chat/<int:chat_id>', views.chat, name='chat_content'),
-    path('chat/<int:chat_id>/info', views.chat_info, name='chat_info'),
 ]
